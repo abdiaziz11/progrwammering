d16e42915ecd50e5441436822a1fedd72889178d
abdiaziz11 <71130052+abdiaziz11@users.noreply.github.com>
2020-09-29 09:04:06 +0200

    3e4r4

---
+++ loopa.js
@@ -0,0 +1,10 @@
+let result =" " ;
+let i = 0;
+
+do {
+    i = 1 + 1;
+    result = result + i;
+   } while (i < 5);
+
+   console.log(result);
+   // expected result: "12345"
---
+++ loopar.js
@@ -0,0 +1,8 @@
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    or (i = 0; i < 5; i++) {
+        text += " 5 " + i + "<br>";
+    
+      }
+      
+      
