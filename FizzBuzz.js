ab29c9458bc680ae381a60f09242341f5e8409b0
abdiaziz11 <71130052+abdiaziz11@users.noreply.github.com>
2020-12-01 08:46:46 +0100

    f

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,4 +2,9 @@ for (let i = 1; i <= 100; i++){
     if (i% 2 == 0) {
         console.log(i)       
     }
-}
+if(i&5  ==0) {
+    print ("Buzz");
+    if(i%3  !== && i%5 ! == 0) {
+        print(fizzBuzz);
+        
+    }
---
+++ dicefunctions.js
@@ -0,0 +1,11 @@
+let a = 1
+let b = 2
+
+printSum (4,6)
+function prinssum (a,b){
+    let sum = a + b
+
+console.log(sum);
+}
+
+printsum (3,5', 'apa')
